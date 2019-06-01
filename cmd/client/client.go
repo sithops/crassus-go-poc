be69dd2cb51040cd65c1a18dd41dc17f59379283
Bren Briggs <code@fraq.io>
2019-05-31 21:27:45 -0400

    WIP: Mock client

---
+++ cmd/client/client.go
@@ -0,0 +1,8 @@
+// Copyright © 2019 Bren Briggs <code@fraq.io>
+
+// This package mocks clients used to demonstrate and test the Crassus router
+package main
+
+import (
+	"github.com/c-bata/go-prompt"
+)
